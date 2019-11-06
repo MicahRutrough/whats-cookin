f64429fdb5908e63ac679803667c0c6a3aed2d18
Micah Rutrough <mrutroug@uci.edu>
2019-11-06 11:33:21 -0800

    Hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+app.get('/', function (request, response) {
+	response.send('<h1>Hello, World!</h1>');
+});
+
+http.listen(3000, function() {
+	console.log("Listening on *:3000");
+});
